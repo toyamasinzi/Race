1a12a3e0c9228a18cd27e4088056b0fc9e1922ee
sinzitoyama <yamat422@gmail.com>
2022-09-05 19:30:11 +0900

    曲がる

--- Assets/Script/Car.cs
+++ Assets/Script/Car.cs
@@ -12,6 +12,8 @@ public class Car : MonoBehaviour
     private float _leftRt = 0f;
     private float _rightRt = 0f;
     public float _vInput = 0f;
+    public float _hInput = 0f;
+
     public float _moveSpeed = 0.5f;
     [SerializeField] float _maxSpeed = 5f;
 
@@ -34,6 +36,10 @@ public class Car : MonoBehaviour
     {
 
         _vInput = Input.GetAxisRaw("Vertical");
+        _hInput = Input.GetAxisRaw("Horizontal");
+
+
+        transform.Rotate(0, _hInput, 0);
         //Vector3 _dir = new Vector3(0, 0, _vInput);// z軸にVerticalの入力を代入
         if (_vInput != 0 && _measurement == false)
         {
@@ -46,46 +52,47 @@ public class Car : MonoBehaviour
         {
             _speed = 0f;
         }
-        _rb.velocity = transform.right * _speed;
+        _rb.velocity = transform.forward * _speed;
         //_rb.AddForce(_rb.mass * Vector3.right * _speed / Time.fixedDeltaTime, ForceMode.Force);
 
 
         //_camera.transform.position = transform.position;
         //// _rb.velocity = new Vector3(_camera.forward.x, _rb.velocity.y, _camera.forward.z) * _speed;
         //Vector3 vec = this.transform.right; //Camera.main.transform.TransformDirection(_dir);//カメラをローカル座標からワールド座標に変換し、代入した入力をカメラ基準にする
-        //                                    //vec.y = 0;//y軸を固定
-        //                                    //vec = vec.normalized;//速度を一定化
+        //vec.y = 0;//y軸を固定
+        //vec = vec.normalized;//速度を一定化
         //_rb.velocity = vec * _speed;//移動
 
-        if (Input.GetKey("a"))//左に回転
-        {
-            _dirRt = true;
-            if (_dirRt)
-            {
-                _leftRt -= Time.deltaTime * _rtSpeed;
-            }
-            transform.Rotate(0, _leftRt, 0);
-        }
-        else
-        {
-            _leftRt = 0f;
-            _dirRt = false;
-        }
 
-        if (Input.GetKey("d"))//右に回転
-        {
-            _dirRt = true;
-            if (_dirRt)
-            {
-                _rightRt += Time.deltaTime * _rtSpeed;
-            }
-            transform.Rotate(0, _rightRt, 0);
-        }
-        else
-        {
-            _dirRt = false;
-            _rightRt = 0f;
-        }
+        //if (Input.GetKey("a"))//左に回転
+        //{
+        //    _dirRt = true;
+        //    if (_dirRt)
+        //    {
+        //        _leftRt -= Time.deltaTime * _rtSpeed;
+        //    }
+        //    transform.Rotate(0, _leftRt, 0);
+        //}
+        //else
+        //{
+        //    _leftRt = 0f;
+        //    _dirRt = false;
+        //}
+
+        //if (Input.GetKey("d"))//右に回転
+        //{
+        //    _dirRt = true;
+        //    if (_dirRt)
+        //    {
+        //        _rightRt += Time.deltaTime * _rtSpeed;
+        //    }
+        //    transform.Rotate(0, _rightRt, 0);
+        //}
+        //else
+        //{
+        //    _dirRt = false;
+        //    _rightRt = 0f;
+        //}
 
 
         /*  float handle = GetHandleInput();
---
+++ Assets/Script/Handl.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Handl : MonoBehaviour
+{
+    [SerializeField] float a = 0f;
+   
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Handling();
+    }
+    void Handling()
+    {
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+
+        // 入力方向のベクトルを組み立てる
+        Vector3 dir = Vector3.forward * v + Vector3.right * h;
+
+        if (dir  != Vector3.zero)
+        {
+            // カメラを基準に入力が上下=奥/手前, 左右=左右にキャラクターを向ける
+            dir = Camera.main.transform.TransformDirection(dir);    // メインカメラを基準に入力方向のベクトルを変換する
+            dir.y = 0;  // y 軸方向はゼロにして水平方向のベクトルにする
+
+            // 入力方向に滑らかに回転させる
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, targetRotation, Time.deltaTime * a);
+        }
+    }
+}
+        
